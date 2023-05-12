1a53d4f803c5f2b8ae22bf276c419e4002704988
benzo1312 <YuliyaTsareva743@gmail.com>
2023-05-13 02:15:25 +0300

    homework

---
+++ Task2.py
@@ -0,0 +1,5 @@
+
+length = 250
+velocity = 80
+time = length / velocity
+print("Время =", time)
---
+++ Task5.py
@@ -0,0 +1,5 @@
+import math
+r = 2.5
+k = math.pi
+s = k * r**2
+print("Площать круга =", round(s, 2))
